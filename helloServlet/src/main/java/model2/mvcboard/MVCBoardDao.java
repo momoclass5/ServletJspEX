feffd7be4a43ad7123adc2d7a0a53de0a679ce63
momo <momo.class5@gmail.com>
2023-06-23 12:48:49 +0900

    페이지 처리

--- helloServlet/src/main/java/model2/mvcboard/MVCBoardDao.java
+++ helloServlet/src/main/java/model2/mvcboard/MVCBoardDao.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import common.DBConnPool;
+import dto.Criteria;
 
 public class MVCBoardDao {
 
@@ -23,11 +24,20 @@ public class MVCBoardDao {
 	}
 	
 	// 목록 조회
-	public List<MVCBoardDto> getList(){
+	public List<MVCBoardDto> getList(Criteria cri){
 		List<MVCBoardDto> list = new ArrayList<MVCBoardDto>();
-		String sql = "SELECT * FROM mvcboard ORDER BY idx DESC";
+		String sql = "SELECT * FROM mvcboard ";
 		
+		// 검색어가 null이 아니라면 검색 기능을 추가
+		if(cri.getSearchWord() != null 
+				&& !cri.getSearchWord().equals("")){
+			sql += "WHERE " + cri.getSearchField() 
+					+ " Like '%" + cri.getSearchWord() +"%'";
+		}
 		
+		sql += "ORDER BY idx DESC";
+		
+		//System.out.println("검색 sql : " + sql);
 		try (Connection conn = DBConnPool.getConnection();
 				PreparedStatement psmt = conn.prepareStatement(sql);){
 			
@@ -55,6 +65,89 @@ public class MVCBoardDao {
 		}
 		
 		return list;
+	}
+	
+	// 목록 조회 + 페이징
+	public List<MVCBoardDto> getListPage(Criteria cri){
+		List<MVCBoardDto> list = new ArrayList<MVCBoardDto>();
+		String sql = 
+				"select * from ("
+				+ "	    select tb.*, rownum rn from ("
+				+ "			SELECT * FROM mvcboard ";
+		
+		// 검색어가 null이 아니라면 검색 기능을 추가
+		if(cri.getSearchWord() != null 
+				&& !cri.getSearchWord().equals("")){
+			sql += "WHERE " + cri.getSearchField() 
+					+ " Like '%" + cri.getSearchWord() +"%'";
+		}
+		
+		sql += "			ORDER BY idx DESC"
+				+ "		) tb"
+				+ ")"
+				+ "where rn between ? and ?";
+		
+		System.out.println("검색 sql : " + sql);
+		
+		
+		try (Connection conn = DBConnPool.getConnection();
+				PreparedStatement psmt = conn.prepareStatement(sql);){
+			
+			// 페이징 처리 - 시작번호와 끝번호를 입력 
+			psmt.setInt(1, cri.getStartNo());
+			psmt.setInt(2, cri.getEndNo());
+			
+			ResultSet rs = psmt.executeQuery();
+			while(rs.next()) {
+				MVCBoardDto dto = new MVCBoardDto(
+											rs.getString("idx")
+											, rs.getString("name")
+											, rs.getString("title")
+											, rs.getString("content")
+											, rs.getString("postdate")
+											, rs.getString("ofile")
+											, rs.getString("sfile")
+											, rs.getInt("downcount")
+											, rs.getString("pass")
+											, rs.getInt("visitcount")
+									);
+				
+				list.add(dto);
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return list;
+	}
+
+	public int getTotalCnt(Criteria criteria) {
+		int totalCnt = 0;
+		String sql = "select count(*) "
+					+ "from mvcboard ";
+		if(criteria.getSearchWord() != null && !"".equals(criteria.getSearchWord())) {
+			sql += "where "+ criteria.getSearchField() +" like '%"+ criteria.getSearchWord() +"%'";
+		}	
+		
+		sql += "order by idx desc";
+		
+		System.out.println(sql);
+		
+		try (Connection conn = DBConnPool.getConnection();
+				PreparedStatement psmt = conn.prepareStatement(sql);){
+			ResultSet rs = psmt.executeQuery();
+			rs.next();
+			totalCnt = rs.getInt(1); // 첫번째 컬럼의 값을 반환
+			
+			rs.close();
+		} catch (SQLException e) {
+			System.out.println("총 게시물의 수를 조회 하던중 예외가 발생 하였습니다.");
+			e.printStackTrace();
+		}
+		
+		return totalCnt;
 	}
 }
 
--- helloServlet/src/main/webapp/14MVCBoard/List.jsp
+++ helloServlet/src/main/webapp/14MVCBoard/List.jsp
@@ -8,13 +8,81 @@
 <title>Insert title here</title>
 </head>
 <body>
+
 	<h2>MVC 모델2 게시판</h2>
 	
-	<c:forEach items="${list }" var="row" varStatus="loop">
-		${row.idx }<br>
-		${row.name }<br>
-		${row.title }<br>
-		================<br>
-	</c:forEach>
+	
+	<h4>총 게시물수 : ${totalCnt }</h4>
+	<!-- 검색 폼 -->
+    <form method="get" name="searchForm">  
+    <table border="1" width="90%">
+    
+    <!-- 페이지 번호 -->
+    <input type="text" name="pageNo">
+    
+    <tr>
+        <td align="center">
+            <select name="searchField">
+                <option value="title">제목</option>
+                <option value="content" 
+                	${param.searchField eq "content" ? "selected" : "" }>
+                	내용</option>
+            </select>
+            <input type="text" name="searchWord" value="${param.searchWord }" />
+            <input type="submit" value="검색하기" />
+        </td>
+    </tr>
+    </table>
+    </form>
+    
+	<!-- 목록 테이블 -->
+	<table border = "1" width="90%">
+		<tr>
+			<th width="10%">번호</th>
+            <th width="*">제목</th>
+            <th width="15%">작성자</th>
+            <th width="10%">조회수</th>
+            <th width="15%">작성일</th>
+            <th width="8%">첨부</th>
+		</tr>
+		
+		<c:choose>
+			<c:when test="${empty list }">
+				<!-- 조회된 게시물이 없는경우 -->
+				<tr>
+					<td colspan="6">등록된 게시물이 없습니다.</td>
+				</tr>
+			</c:when>
+			<c:otherwise>
+				<!-- 게시물 목록 출력 -->
+				<c:forEach items="${list }" var="row" varStatus="loop">
+					<tr>	
+						<td>${row.idx }</td>
+						<td>${row.title }</td>
+						<td>${row.name }</td>
+						<td>${row.visitcount }</td>
+						<td>${row.postdate }</td>
+						<td>첨부파일</td>
+					</tr>
+				</c:forEach>
+			</c:otherwise>
+		</c:choose>
+	
+	</table>
+	
+	<%@include file="../9페이지/PageNavi.jsp" %>
+	
+	
 </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
